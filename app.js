868eedd83e6d10772559a91f7471e77924a0b041
Eric <ekbrown55@gmail.com>
2023-12-05 12:57:49 -0500

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- app.js
+++ app.js
@@ -29,5 +29,57 @@ const $h3 = $('<h3>');
 	$container.append($h4Pet);
 
     const $h4Wand = $('<h4>');
-	$h4Wand.text('elder wand');
+	$h4Wand.text('Elder Wand');
 	$container.append($h4Wand);
+
+    const $ul = $('<ul>').attr('storage', 'trunk');
+
+    const items = [
+        'butter beer',
+        'invisibility cloak',
+        'magic map',
+        'time turner',
+        'lash',
+        "Bertie Bott's Every FLavor[Jelly] Beans"
+
+    ];
+
+    items.forEach(item => {
+        const $li = $('<li>').text(item);
+        if (item === 'invisibility cloak' || item === 'magic map' || item === 'time turner') {
+            $li.addClass('secret');
+        }
+        if (item === 'lash') {
+            $li.addClass('pet-type');
+
+        }
+    });
+
+    $container.append($ul);
+
+    const $h5 = $('<h5>').text('Spring 2017');
+    $container.append($h5);
+
+    const $table = $('<table>');
+
+    const $thead = $('<thead>');
+    const $th1 = $('<th>').text('Day');
+    const $th2 = $('<th>').text('Classes');
+
+    $thead.append($th1, $th2);
+
+    $table.append($thead);
+
+    const days = ['MOnday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday'];
+
+days.forEach(day => {
+    const $tr = $('<tr>');
+    const $tdDay = $('<td>').text(day);
+    const $tdClasses = $('<td>').text('Herbology, Divination, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice')
+
+    $tr.append($tdDay, $tdClasses);
+
+    $table.append($tr);
+
+    $container.append($table);
+});
